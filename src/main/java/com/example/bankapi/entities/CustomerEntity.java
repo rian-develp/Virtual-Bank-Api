73bc561e0643bff702293c2a82cc59d5316fe76f
rian-develp <riandeveloper1@gmail.com>
2025-06-02 10:30:55 -0300

    Apagando anotações desnecessárias do lombok

--- src/main/java/com/example/bankapi/entities/CustomerEntity.java
+++ src/main/java/com/example/bankapi/entities/CustomerEntity.java
@@ -10,24 +10,17 @@ import lombok.*;
 @Table(name = "customers")
 @Getter
 @Setter
-@RequiredArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerEntity {
-
     @Id
     @Column(length = 11, nullable = false)
-    @NonNull
     private String cpf;
     @Column(length = 33, nullable = false, unique = true)
-    @NonNull
     private String email;
     @Column(length = 44, nullable = false)
-    @NonNull
     private String name;
     @Column(name = "phone_number", length = 11, nullable = false, unique = true)
-    @NonNull
     private String phoneNumber;
     @Column(length = 12, nullable = false)
-    @NonNull
     private String password;
 }
